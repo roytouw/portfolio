d029cb7b09e586dfd47b455f2ecc92b16848010e
Roy Touw <roytouw@hotmail.com>
2016-06-30 21:41:43 +0200

    project cycler added

--- app/scripts/controllers/project.js
+++ app/scripts/controllers/project.js
@@ -3,7 +3,43 @@
  */
 'use strict';
 
-angular.module('portfolioApp').controller('ProjectCtrl', ['$scope', function($scope) {
-  $scope.title = 'Multopy';
-  $scope.image = 'plaatje';
+angular.module('portfolioApp').controller('ProjectCtrl', ['$scope', '$interval', function($scope, $interval) {
+  $scope.projectIterator = 0;
+  $scope.current = {
+    title: 'Multopy',
+    image: 'plaatjeMultopy'
+  };
+
+  $scope.projects = [
+    {
+      title: 'Multopy',
+      image: 'plaatje'
+    }, {
+      title: 'Sesam',
+      image: 'plaatje2'
+    }, {
+      title: 'WFPCS',
+      image: 'plaatje3'
+    }
+  ];
+
+  $scope.startCycling = function() {
+    $interval(function() {
+      $scope.nextProject();
+    }, 7000);
+  };
+
+  $scope.nextProject = function() {
+    $scope.current = $scope.projects[$scope.iterateProjects()];
+  };
+
+  $scope.iterateProjects = function() {
+    if($scope.projectIterator + 1 < $scope.projects.length)
+      $scope.projectIterator += 1;
+    else
+      $scope.projectIterator = 0;
+    return $scope.projectIterator;
+  };
+  $scope.startCycling();
+
 }]);
