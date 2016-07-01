91bddd0a2d3b2e6198b0175bd803a555dde2409e
Roy Touw <roytouw@hotmail.com>
2016-07-01 15:34:58 +0200

    Highlight made readable

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,5 +4,7 @@
   Main Controller.
  */
 angular.module('portfolioApp').controller('MainCtrl', ['$scope', function($scope) {
-  $scope.class = 'btn-linkedin-hover';
+  $scope.slideProject = function() {
+    ngAnimate
+  };
 }]);
